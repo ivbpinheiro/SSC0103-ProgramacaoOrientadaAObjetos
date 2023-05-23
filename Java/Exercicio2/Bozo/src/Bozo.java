22f1131f3abe71e63118bd9dd083575032ea9157
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2023-05-22 23:05:59 -0300

    Ajustado Random 1 a 6

--- Java/Exercicio2/Bozo/src/Bozo.java
+++ Java/Exercicio2/Bozo/src/Bozo.java
@@ -1,13 +1,16 @@
 public class Bozo {
     public static void main(java.lang.String[] args) throws java.io.IOException{
-        Random rd = new Random(10);
-        System.out.println(rd.getRandomNumber());
-        System.out.println(rd.getSemente());
-        System.out.println(rd.getIntRand(3));
-        System.out.println(rd.getRand());
-        rd.setSemente(100);
-        System.out.println(rd.getSemente());
+//        Random rd = new Random(10);
+//        System.out.println(rd.getRandomNumber());
+//        System.out.println(rd.getSemente());
+//        System.out.println(rd.getIntRand(3));
+//        System.out.println(rd.getRand());
+//        rd.setSemente(100);
+//        System.out.println(rd.getSemente());
         RolaDados rola = new RolaDados(6);
         System.out.println(rola.getListaDeDados());
+        Dado dd = new Dado();
+        //System.out.println(dd.rolar());
+        System.out.println(dd);
     }
 }
--- Java/Exercicio2/Bozo/src/Dado.java
+++ Java/Exercicio2/Bozo/src/Dado.java
@@ -1,19 +1,37 @@
+import java.util.ArrayList;
+
 public class Dado {
+    ArrayList<String> lados;
 
     private int numLados = 6;
 
-    private int ladoSelecionado = -1;
+    private Random ladoSelecionado;
 
-    public Dado(int n, int randomNumber){
+    public Dado(){
+
+    }
+    public Dado(int n){
         this.numLados = n;
     }
 
+    public int getLado(){
+        return ladoSelecionado.getRandomNumber();
+    }
+
     public int rolar(){
-        return 0;
+        ladoSelecionado = new Random();
+        return ladoSelecionado.getRandomNumber();
     }
 
     public java.lang.String toString(){
-        return "-";
+        lados = new ArrayList<>();
+        lados.add("\n1\n" +
+                "+-----+\n" +
+                "|     |\n" +
+                "|  *  |\n" +
+                "|     |\n" +
+                "+-----+");
+        return lados.get(0);
     }
 
     public static void main(java.lang.String[] args){
