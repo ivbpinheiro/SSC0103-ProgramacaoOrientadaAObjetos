32284edab22cb3f458e22548108fc7945f481595
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2023-05-24 23:04:30 -0300

    corrigido main

--- Java/Exercicio2/Bozo/src/Bozo.java
+++ Java/Exercicio2/Bozo/src/Bozo.java
@@ -7,29 +7,31 @@ public class Bozo {
         System.out.println("Bem-vindx ao Bozó!!! ");
         System.out.println("Siga as instruções abaixo:");
         System.out.println();
-        for (int i = 0; i < 10; i++) {
-            while (true) {
-                try {
-                    System.out.println("Pressione ENTER para iniciar a rodada: ");
-                    entrada = EntradaTeclado.leString();
-                    if (entrada.isEmpty()) break;
-                } catch (Exception eIn) {
-                    System.out.println("Entrada inválida. Digite novamente.");
-                }
+
+        int rodadas = 10;
+        int contador = 0;
+        while (contador < rodadas) {
+            System.out.println("Pressione ENTER para iniciar a rodada: ");
+            entrada = EntradaTeclado.leString();
+            if (!entrada.isEmpty()) {
+                contador--;
+                continue;
             }
 
             dados = r.rolar();
             System.out.println(r.toString());
 
             for (int j = 0; j < 2; j++) {
-                try {
-                    System.out.println("Escolha os dados que serão rolados (coloque os números espaçados)");
-                    System.out.println("ou aperte ENTER para permanecer com os dados atuais: ");
-                    entrada = EntradaTeclado.leString();
-                    if (!entrada.isEmpty()) dados = r.rolar(entrada);
-                } catch (Exception eIn) {
-                    System.out.println("Inválido! Digite novamente.");
-                    j--;
+                System.out.println("Escolha os dados que serão rolados (coloque os números espaçados)");
+                System.out.println("ou aperte ENTER para permanecer com os dados atuais: ");
+                entrada = EntradaTeclado.leString();
+                if (!entrada.isEmpty()) {
+                    try {
+                        dados = r.rolar(entrada);
+                    } catch (Exception eIn) {
+                        System.out.println("Inválido! Digite novamente.");
+                        j--;
+                    }
                 }
                 System.out.println(r.toString());
             }
@@ -37,20 +39,17 @@ public class Bozo {
             System.out.println(p.toString());
 
             while (true) {
-                try {
-                    System.out.print("Digite a posição que você quer ocupar: ");
-                    int posIn = EntradaTeclado.leInt();
-                    if (posIn > 0) {
-                        p.add(posIn, dados);
-                        break;
-                    }
-                    System.out.println("Inválido! Digite novamente.");
-                } catch (Exception eIn) {
-                    System.out.println("Inválido! Digite novamente.");
+                System.out.print("Digite a posição que você quer ocupar: ");
+                int posIn = EntradaTeclado.leInt();
+                if (posIn > 0) {
+                    p.add(posIn, dados);
+                    break;
                 }
+                System.out.println("Inválido! Digite novamente.");
             }
 
             System.out.println(p.toString());
+            contador++;
         }
 
         System.out.println("Placar final: " + p.getScore());
